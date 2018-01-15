dc8772c9fd1352931d76c77df8354b3bd21c1dc3
Michel Vielmetter <mvielmetter@rsteinmann.de>
2018-01-15 12:19:43 +0100

    Bootstrap finished

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -1,6 +1,10 @@
+<!DOCTYPE html>
 <html>
 <head>
-
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script href="{{ mix('js/app.js') }}"></script>
+    <title>lecture table</title>
 </head>
 <body>
 
