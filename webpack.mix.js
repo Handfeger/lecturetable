3c664e99f9706978db19b8e784ad95f201014558
Michel Vielmetter <mvielmetter@rsteinmann.de>
2018-01-16 12:40:42 +0100

    whoops

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix
-    .ts('resources/assets/js/app.js', 'public/js')
+    .ts('resources/assets/ts/app.ts', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
 
